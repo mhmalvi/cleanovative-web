4fbb7d01c6d55c0ae3426f372554818d26ed22b8
anrajin <an.rajin@gmail.com>
2021-11-07 21:31:01 +0600

    updated

--- resources/views/components/home/slider.blade.php
+++ resources/views/components/home/slider.blade.php
@@ -7,7 +7,7 @@
                     <span class="promo-text">
                         Home Cleaning
                     </span>
-                    <h1>Sydney’s No 1 rated <br><span>trusted cleaners for your home</span>
+                    <h1 class="slider-title">Sydney’s No 1 rated <br><span>trusted cleaners for your home</span>
                     </h1>
                     <div class="btn-wrap">
                         <a class="main-btn" href="contact.html">Quote Me<i class="fal fa-plus"></i></a>
@@ -23,7 +23,7 @@
                     <span class="promo-text">
                         Commercial Cleaning
                     </span>
-                    <h1><span>The Only Choice of</span> Commercial Cleaners</h1>
+                    <h1 class="slider-title"><span>The Only Choice of</span> Commercial Cleaners</h1>
                     <div class="btn-wrap">
                         <a class="main-btn" href="contact.html">Contact Us<i class="fal fa-plus"></i></a>
                         <a class="main-btn main-btn-2" href="services.html">Quote Me<i
