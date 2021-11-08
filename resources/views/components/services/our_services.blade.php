0919b86737187551a8e9b0597277ff49325176f7
Zakaria <jakariablaine120@gmail.com>
2021-11-08 16:12:01 +0600

    Update some UIs

--- resources/views/components/services/our_services.blade.php
+++ resources/views/components/services/our_services.blade.php
@@ -9,37 +9,101 @@
         <div class="row service-loop">
             <div class="col-lg-4 col-md-6 wow fadeInLeft" data-wow-delay=".3s">
                 <div class="single-service">
-                    <div class="service-bg" style="background-image: url(assets/img/service-1.jpg);">
+                    <div class="service-bg" style="background-image: url({{ asset('images/service-1.jpg') }});">
                         <span class="service-count">01</span>
-                        <a href="#" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                        <a href="{{ route('services.details', ['number' => '1']) }}" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
                     </div>
                     <div class="service-content">
-                        <h4>House Claning</h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus errsit voluptatem accus</p>
+                        <h4>House-Residential Cleaning</h4>
+                        <p>
+                            Grab a coffee with your loved ones while we handle the housecleaning so you may walk into your home that has been inexplicably cleaned to your satisfaction.
+                        </p>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay=".5s">
                 <div class="single-service active">
-                    <div class="service-bg" style="background-image: url(assets/img/service-2.jpg);">
-                        <span class="service-count">01</span>
-                        <a href="#" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                    <div class="service-bg" style="background-image: url({{ asset('images/service-2.jpg') }});">
+                        <span class="service-count">02</span>
+                        <a href="{{ route('services.details', ['number' => '2']) }}" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
                     </div>
                     <div class="service-content">
-                        <h4>Kitchen Cleaning</h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus errsit voluptatem accus</p>
+                        <h4>End of Lease Cleaning</h4>
+                        <p>
+                            Moving in-or-out of houses? Looking to secure a strong return on your bond? Don’t stress, You’ve come to the right spot. Allow us to handle ... see more
+                        </p>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
                 <div class=" single-service">
-                    <div class="service-bg" style="background-image: url(assets/img/service-3.jpg);">
-                        <span class="service-count">01</span>
-                        <a href="#" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                    <div class="service-bg" style="background-image: url({{ asset('images/service-3.jpg')}});">
+                        <span class="service-count">03</span>
+                        <a href="{{ route('services.details', ['number' => '3']) }}" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
                     </div>
                     <div class="service-content">
-                        <h4>Window Claning</h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus errsit voluptatem accus</p>
+                        <h4>Office-Commercial Cleaning</h4>
+                        <p>
+                            A lot of time is spent at work, and significant meetings are most likely held there. It is critical to maintain the cleanliness of the office ... see more
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
+                <div class=" single-service">
+                    <div class="service-bg" style="background-image: url({{ asset('images/service-4.jpg')}});">
+                        <span class="service-count">04</span>
+                        <a href="{{ route('services.details', ['number' => '4']) }}" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                    </div>
+                    <div class="service-content">
+                        <h4>Party-Event Cleaning</h4>
+                        <p>
+                            Planning on having a party or an event? Worried about the mess after? You’ve come to the right spot, at Cleanovative we cater for every after ... see more
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
+                <div class=" single-service">
+                    <div class="service-bg" style="background-image: url({{ asset('images/service-5.jpg')}});">
+                        <span class="service-count">05</span>
+                        <a href="{{ route('services.details', ['number' => '5']) }}" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                    </div>
+                    <div class="service-content">
+                        <h4>Construction Cleaning</h4>
+                        <p>
+                            Worried about the mess made after construction work? A post-construction property becomes hazardous for our tradies and you due to the mess made.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
+                <div class=" single-service">
+                    <div class="service-bg" style="background-image: url({{ asset('images/service-6.jpg')}});">
+                        <span class="service-count">06</span>
+                        <a href="{{ route('services.details', ['number' => '6']) }}" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                    </div>
+                    <div class="service-content">
+                        <h4>Strata Cleaning</h4>
+                        <p>
+                            Strata is one of the major maintenance industries for real-estate. At Cleanovative, we make sure our professional team keeps your housing clean and spotless ... see more
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
+                <div class=" single-service">
+                    <div class="service-bg" style="background-image: url({{ asset('images/service-7.jpg')}});">
+                        <span class="service-count">07</span>
+                        <a href="{{ route('services.details', ['number' => '7']) }}" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                    </div>
+                    <div class="service-content">
+                        <h4>Other Commercial Cleaning</h4>
+                        <p>
+                            Apart from the specialist industry-based cleaning, Cleanovative can also provide generalist building cleaning services for all types of Sydney businesses.
+                        </p>
                     </div>
                 </div>
             </div>
--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <!--====== BREADCRUMB PART START ======-->
-    <section class="breadcrumb-area" style="background-image: url(assets/img/breadcrumb.jpg);">
+    <section class="breadcrumb-area" style="background-image: url({{ asset('images/breadcrumb.jpg') }});">
         <div class="container">
             <div class="breadcrumb-text text-center">
                 <h1 class="page-title">FAQ</h1>
@@ -351,72 +351,7 @@
         </div>
     </section>
     <!--====== FAQ PART END ======-->
-    <!--====== BRAND AREA START ======-->
-    <section class="brand-section">
-        <div class="container">
-            <div class="brand-slide-wrap" id="brandSlide">
-                <div class="single-brand">
-                    <div class="brand-img text-center">
-                        <a href="#"><img src="assets/img/brand01.png" alt="Image"></a>
-                    </div>
-                </div>
-                <div class="single-brand">
-                    <div class="brand-img text-center">
-                        <a href="#"><img src="assets/img/brand02.png" alt="Image"></a>
-                    </div>
-                </div>
-                <div class="single-brand">
-                    <div class="brand-img text-center">
-                        <a href="#"><img src="assets/img/brand03.png" alt="Image"></a>
-                    </div>
-                </div>
-                <div class="single-brand">
-                    <div class="brand-img text-center">
-                        <a href="#"><img src="assets/img/brand04.png" alt="Image"></a>
-                    </div>
-                </div>
-                <div class="single-brand">
-                    <div class="brand-img text-center">
-                        <a href="#"><img src="assets/img/brand05.png" alt="Image"></a>
-                    </div>
-                </div>
-                <div class="single-brand">
-                    <div class="brand-img text-center">
-                        <a href="#"><img src="assets/img/brand06.png" alt="Image"></a>
-                    </div>
-                </div>
-                <div class="single-brand">
-                    <div class="brand-img text-center">
-                        <a href="#"><img src="assets/img/brand01.png" alt="Image"></a>
-                    </div>
-                </div>
-                <div class="single-brand">
-                    <div class="brand-img text-center">
-                        <a href="#"><img src="assets/img/brand02.png" alt="Image"></a>
-                    </div>
-                </div>
-                <div class="single-brand">
-                    <div class="brand-img text-center">
-                        <a href="#"><img src="assets/img/brand03.png" alt="Image"></a>
-                    </div>
-                </div>
-                <div class="single-brand">
-                    <div class="brand-img text-center">
-                        <a href="#"><img src="assets/img/brand04.png" alt="Image"></a>
-                    </div>
-                </div>
-                <div class="single-brand">
-                    <div class="brand-img text-center">
-                        <a href="#"><img src="assets/img/brand05.png" alt="Image"></a>
-                    </div>
-                </div>
-                <div class="single-brand">
-                    <div class="brand-img text-center">
-                        <a href="#"><img src="assets/img/brand06.png" alt="Image"></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!--====== BRAND AREA END ======-->
+
+    @include('components.brands')
+
 @endsection
