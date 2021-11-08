98def66aa5ae3e29801d74ecb7fc413351364863
Zakaria <jakariablaine120@gmail.com>
2021-11-08 11:34:01 +0600

    Add services in homepage

--- resources/views/components/home/services.blade.php
+++ resources/views/components/home/services.blade.php
@@ -2,8 +2,10 @@
 <section class="service-section" style="background-image: url({{asset('images/service-bg.png')}});">
     <div class="container">
         <div class="section-title white-color text-center both-border mb-50">
-            <h2>We Provide Best Offer For <br>
-                Cleaning Services <span>02</span></h2>
+            <h2>
+                Are you looking for the right company <br> to make your home or office look <br> as good as new?
+                <span>02</span>
+            </h2>
             <p class="title-tag">Company Services</p>
         </div>
         <div class="row service-loop">
@@ -14,32 +16,96 @@
                         <a href="#" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
                     </div>
                     <div class="service-content">
-                        <h4>House Claning</h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus errsit voluptatem accus</p>
+                        <h4>House-Residential Cleaning</h4>
+                        <p>
+                            Grab a coffee with your loved ones while we handle the housecleaning so you may walk into your home that has been inexplicably cleaned to your satisfaction.
+                        </p>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay=".5s">
                 <div class="single-service active">
                     <div class="service-bg" style="background-image: url(assets/img/service-2.jpg);">
-                        <span class="service-count">01</span>
+                        <span class="service-count">02</span>
                         <a href="#" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
                     </div>
                     <div class="service-content">
-                        <h4>Kitchen Cleaning</h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus errsit voluptatem accus</p>
+                        <h4>End of Lease Cleaning</h4>
+                        <p>
+                            Moving in-or-out of houses? Looking to secure a strong return on your bond? Don’t stress, You’ve come to the right spot. Allow us to handle ... see more
+                        </p>
                     </div>
                 </div>
             </div>
             <div class="col-lg-4 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
                 <div class=" single-service">
                     <div class="service-bg" style="background-image: url(assets/img/service-3.jpg);">
-                        <span class="service-count">01</span>
+                        <span class="service-count">03</span>
                         <a href="#" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
                     </div>
                     <div class="service-content">
-                        <h4>Window Claning</h4>
-                        <p>Sed ut perspiciatis unde omnis iste natus errsit voluptatem accus</p>
+                        <h4>Office-Commercial Cleaning</h4>
+                        <p>
+                            A lot of time is spent at work, and significant meetings are most likely held there. It is critical to maintain the cleanliness of the office ... see more
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
+                <div class=" single-service">
+                    <div class="service-bg" style="background-image: url(assets/img/service-3.jpg);">
+                        <span class="service-count">04</span>
+                        <a href="#" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                    </div>
+                    <div class="service-content">
+                        <h4>Party-Event Cleaning</h4>
+                        <p>
+                            Planning on having a party or an event? Worried about the mess after? You’ve come to the right spot, at Cleanovative we cater for every after ... see more
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
+                <div class=" single-service">
+                    <div class="service-bg" style="background-image: url(assets/img/service-3.jpg);">
+                        <span class="service-count">05</span>
+                        <a href="#" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                    </div>
+                    <div class="service-content">
+                        <h4>Construction Cleaning</h4>
+                        <p>
+                            Worried about the mess made after construction work? A post-construction property becomes hazardous for our tradies and you due to the mess made.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
+                <div class=" single-service">
+                    <div class="service-bg" style="background-image: url(assets/img/service-3.jpg);">
+                        <span class="service-count">06</span>
+                        <a href="#" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                    </div>
+                    <div class="service-content">
+                        <h4>Strata Cleaning</h4>
+                        <p>
+                            Strata is one of the major maintenance industries for real-estate. At Cleanovative, we make sure our professional team keeps your housing clean and spotless ... see more
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
+                <div class=" single-service">
+                    <div class="service-bg" style="background-image: url(assets/img/service-3.jpg);">
+                        <span class="service-count">07</span>
+                        <a href="#" class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                    </div>
+                    <div class="service-content">
+                        <h4>Other Commercial Cleaning</h4>
+                        <p>
+                            Apart from the specialist industry-based cleaning, Cleanovative can also provide generalist building cleaning services for all types of Sydney businesses.
+                        </p>
                     </div>
                 </div>
             </div>
