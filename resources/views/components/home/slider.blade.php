a833a067b81cfd019a21391941d94b460894b315
Zakaria <jakariablaine120@gmail.com>
2021-11-08 17:58:51 +0600

    Fix text color

--- resources/views/components/home/slider.blade.php
+++ resources/views/components/home/slider.blade.php
@@ -7,7 +7,7 @@
                     <span class="promo-text">
                         Home Cleaning
                     </span>
-                    <h1 class="slider-title">Sydney’s No 1 rated <br><span>trusted cleaners for your home</span>
+                    <h1 class="slider-title text-dark">Sydney’s No 1 rated <br><span class="text-dark">trusted cleaners for your home</span>
                     </h1>
                     <div class="btn-wrap">
                         <a class="main-btn" href="{{ route('contact') }}">Quote Me<i class="fal fa-plus"></i></a>
@@ -23,7 +23,7 @@
                     <span class="promo-text">
                         Commercial Cleaning
                     </span>
-                    <h1 class="slider-title"><span>The Only Choice of</span> Commercial Cleaners</h1>
+                    <h1 class="slider-title text-dark"><span class="text-dark">The Only Choice of</span> Commercial Cleaners</h1>
                     <div class="btn-wrap">
                         <a class="main-btn" href="{{ route('contact') }}">Contact Us<i class="fal fa-plus"></i></a>
                         <a class="main-btn main-btn-2" href="{{ route('services') }}">Quote Me<i
