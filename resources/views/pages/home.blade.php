b72ca3c18083a30664e705aea596cda47e11c666
ANrajin <dev.quadque@gmail.com>
2021-11-10 12:32:08 +0600

    ui updated

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -20,6 +20,15 @@
 
     @include('components.home.about_section')
 
+    <section>
+        <div class="banner-new">
+            <h2 class="banner-title pb-3">We provide whatever you need for cleaning service</h2>
+            <p class="banner-para">
+                So just grab a coffee and sit back, we will take take care of everything
+            </p>
+        </div>
+    </section>
+
     @include('components.home.appointment')
 
 @endsection
