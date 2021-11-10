61af3909ea124a101d578dec1aad46beb062aef5
Zakaria <jakariablaine120@gmail.com>
2021-11-10 16:27:01 +0600

    Fix services grid layout

--- resources/views/components/services/our_services.blade.php
+++ resources/views/components/services/our_services.blade.php
@@ -7,94 +7,122 @@
             <p class="title-tag">Company Services</p>
         </div>
         <div class="row service-loop">
-            <div class="col-lg-3 col-md-6 wow fadeInLeft" data-wow-delay=".3s">
-                <div class="single-service">
-                    <div class="service-bg" style="background-image: url({{ asset('images/service-1.jpg') }});">
-                        <a href="{{ route('service.strata_cleaning') }}"
-                            class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+            <div class="col-lg-3 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".3s">
+                <div class=" single-service">
+                    <div class="service-bg">
+                        <img src="{{ asset('images/service-6.jpg') }}" alt="service" class="img-fluid rounded-top">
+                        <a href="{{ route('service.house_cleaning') }}"
+                            class="service-link">
+                            <img src="{{asset('images/icons/house.png')}}" alt="">
+                        </a>
                     </div>
-                    <div class="service-content">
-                        <h6>Strata Cleaning</h6>
+                    <div class="service-content rounded-bottom">
+                        <h6>House Cleaning</h6>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay=".5s">
-                <div class="single-service active">
-                    <div class="service-bg" style="background-image: url({{ asset('images/service-2.jpg') }});">
-                        <a href="{{ route('service.end_of_lease_cleaning') }}"
-                            class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+
+            <div class="col-lg-3 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".3s">
+                <div class=" single-service">
+                    <div class="service-bg">
+                        <img src="{{ asset('images/service-8.jpg') }}" alt="service" class="img-fluid rounded-top">
+                        <a href="{{ route('service.deep_cleaning') }}"
+                            class="service-link">
+                            <img src="{{asset('images/icons/carpet-cleaner.png')}}" alt="">
+                        </a>
                     </div>
-                    <div class="service-content">
+                    <div class="service-content rounded-bottom">
+                        <h6>Deep Cleaning</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 wow fadeInRight" data-wow-delay=".5s">
+                <div class="single-service active">
+                    <div class="service-bg">
+                        <img src="{{ asset('images/service-2.jpg') }}" alt="service" class="img-fluid rounded-top">
+                        <a href="{{ route('service.end_of_lease_cleaning') }}"
+                            class="service-link">
+                            <img src="{{asset('images/icons/sanitizer.png')}}" alt="">
+                        </a>
+                    </div>
+                    <div class="service-content rounded-bottom">
                         <h6>End of Lease Cleaning</h6>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
+
+            <div class="col-lg-3 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".5s">
                 <div class=" single-service">
-                    <div class="service-bg" style="background-image: url({{ asset('images/service-3.jpg') }});">
-                        <a href="{{ route('service.construction_cleaning') }}"
-                            class="service-link"><i class="fal fa-long-arrow-right"></i></a>
-                    </div>
-                    <div class="service-content">
-                        <h6>Construction Cleaning</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
-                <div class=" single-service">
-                    <div class="service-bg" style="background-image: url({{ asset('images/service-4.jpg') }});">
+                    <div class="service-bg">
+                        <img src="{{ asset('images/service-4.jpg') }}" alt="service" class="img-fluid rounded-top">
                         <a href="{{ route('service.party_event_cleaning') }}"
-                            class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                            class="service-link">
+                            <img src="{{asset('images/icons/cleaning-cart.png')}}" alt="">
+                        </a>
                     </div>
-                    <div class="service-content">
+                    <div class="service-content rounded-bottom">
                         <h6>Party-Event Cleaning</h6>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
+
+            <div class="col-lg-3 col-md-6 mx-auto wow fadeInUp" data-wow-delay=".7s">
                 <div class=" single-service">
-                    <div class="service-bg" style="background-image: url({{ asset('images/service-5.jpg') }});">
+                    <div class="service-bg">
+                        <img src="{{ asset('images/service-5.jpg') }}" alt="service" class="img-fluid rounded-top">
                         <a href="{{ route('service.office_cleaning') }}"
-                            class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                            class="service-link">
+                            <img src="{{asset('images/icons/cleaning.png')}}" alt="">
+                        </a>
                     </div>
-                    <div class="service-content">
+                    <div class="service-content rounded-bottom">
                         <h6>Office Cleaning</h6>
                     </div>
                 </div>
             </div>
 
-            <div class="col-lg-3 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
+            <div class="col-lg-3 col-md-6 mx-auto wow fadeInUp" data-wow-delay=".7s">
                 <div class=" single-service">
-                    <div class="service-bg" style="background-image: url({{ asset('images/service-6.jpg') }});">
-                        <a href="{{ route('service.house_cleaning') }}"
-                            class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                    <div class="service-bg">
+                        <img src="{{ asset('images/service-3.jpg') }}" alt="service" class="img-fluid rounded-top">
+                        <a href="{{ route('service.construction_cleaning') }}"
+                            class="service-link">
+                            <img src="{{asset('images/icons/street.png')}}" alt="">
+                        </a>
                     </div>
-                    <div class="service-content">
-                        <h6>Residential Cleaning</h6>
+                    <div class="service-content rounded-bottom">
+                        <h6>Construction Cleaning</h6>
                     </div>
                 </div>
             </div>
 
-            <div class="col-lg-3 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
+            <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay=".7s">
+                <div class="single-service">
+                    <div class="service-bg">
+                        <img src="{{ asset('images/service-1.jpg') }}" alt="service" class="img-fluid rounded-top">
+                        <a href="{{ route('service.strata_cleaning') }}"
+                            class="service-link">
+                            <img src="{{asset('images/icons/mop.png')}}" alt="">
+                        </a>
+                    </div>
+                    <div class="service-content rounded-bottom">
+                        <h6>Strata Cleaning</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mx-auto wow fadeInUp" data-wow-delay=".7s">
                 <div class=" single-service">
-                    <div class="service-bg" style="background-image: url({{ asset('images/service-7.jpg') }});">
+                    <div class="service-bg">
+                        <img src="{{ asset('images/service-7.jpg') }}" alt="service" class="img-fluid rounded-top">
                         <a href="{{ route('service.other_cleaning') }}"
-                            class="service-link"><i class="fal fa-long-arrow-right"></i></a>
+                            class="service-link">
+                            <img src="{{asset('images/icons/spray-bottle.png')}}" alt="">
+                        </a>
                     </div>
-                    <div class="service-content">
+                    <div class="service-content rounded-bottom">
                         <h6>Other Cleaning</h6>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6 mx-auto wow fadeInRight" data-wow-delay=".7s">
-                <div class=" single-service">
-                    <div class="service-bg" style="background-image: url({{ asset('images/service-8.jpg') }});">
-                        <a href="{{ route('service.deep_cleaning') }}"
-                            class="service-link"><i class="fal fa-long-arrow-right"></i></a>
-                    </div>
-                    <div class="service-content">
-                        <h6>Deep Cleaning</h6>
                     </div>
                 </div>
             </div>
