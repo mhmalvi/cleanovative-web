d1e2dce70c74f0bc432c2e0885bfd0ade2a24064
ANrajin <dev.quadque@gmail.com>
2021-11-28 11:22:32 +0600

    asset compilation updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,7 @@
         @include('layouts.footer')
     </div>
 
-    <script src="{{asset('js/app.js')}}" type="text/javascript" defer></script>
+    <script src="{{mix('js/app.js')}}" type="text/javascript" defer></script>
 
     @include('layouts.scripts')
 
