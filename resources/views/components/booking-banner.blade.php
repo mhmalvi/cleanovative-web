e032d78a3ba6c5d842b076bce3c4557489c091f9
ANrajin <dev.quadque@gmail.com>
2021-12-02 16:09:36 +0600

    updated

--- resources/views/components/booking-banner.blade.php
+++ resources/views/components/booking-banner.blade.php
@@ -1,5 +1,5 @@
 <section>
-    <div class="banner-new">
+    <div class="banner-new" style="background-image: url({{asset('images/banner101.webp')}})">
         <h2 class="banner-title pb-3">We provide whatever you need for cleaning service</h2>
         <p class="banner-para">
             So just grab a coffee and sit back, we will take take care of everything
